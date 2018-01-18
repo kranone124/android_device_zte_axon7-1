d14874fcc4c40293d0fb53d5c696d01e4c14603f
LuK1337 <priv.luk@gmail.com>
2018-01-19 00:01:59 +0100

    axon7: Update radio/IMS blobs and do some cleaning
     * From tissot 8.0.0 OPR1.170623.026-8.1.10
    
    Change-Id: I453d2dadde0e80d20efc4b61dff97b07c369a416
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -178,6 +178,7 @@ PRODUCT_PACKAGES += \
     copybit.msm8996 \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
+    libqdMetaData.system \
     libtinyxml \
     memtrack.msm8996 \
     libdisplayconfig \
