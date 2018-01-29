12a399d0509264b12bc12e0a7d34c44f273ffb46
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-01-29 15:52:13 +0100

    Revert "axon7: Add rild socket shim"
    This reverts commit 2fedaeaf2f2f78c016cc3cfc59c49fa9defbb6f7.

--- device.mk
+++ device.mk
@@ -325,8 +325,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl \
-    libxml2 \
-    libshims_rild_socket
+    libxml2
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
